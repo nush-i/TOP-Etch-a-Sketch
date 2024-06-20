1a35ae23a7b7f7599c3a93b2d76b3ccca4974e23
Anushki Kulasekara <171895216+nush-i@users.noreply.github.com>
2024-06-20 17:48:47 +1000

    implemented grid size change

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 var slider = document.getElementById("choseGrid");
 var outputSlider = document.getElementById("chosen-size");
-outputSlider.innerHTML = slider.value + " x " + slider.value;; 
+outputSlider.innerHTML = slider.value + " x " + slider.value;
+
 
 slider.oninput = function() {
     outputSlider.innerHTML = this.value + " x " + this.value;
+    createGridItems();
 }
+
 
 var picker = document.getElementById("choseColour");
 var outputPicker = document.getElementById("chosen-colour");
@@ -15,13 +18,37 @@ picker.oninput = function() {
 }
 
 
-
 const container = document.querySelector('.grid-container');
-for (let i=0; i<16 *16; i++){
-    const div = document.createElement('div');
-    div.classList.add('grid-item');
-    container.appendChild(div);
+function createGridItems(){
+    const gridSize = slider.value;
+    const containerSize = 400;
+    const itemSize = containerSize/gridSize;
+    container.innerHTML = '';
+    for (let i=0; i< gridSize * gridSize; i++){
+        const div = document.createElement('div');
+        div.classList.add('grid-item');
+        div.style.width = `${itemSize}px`;
+        div.style.height = `${itemSize}px`;
+        container.appendChild(div);
+    }
 }
+
+
+let isMouseDown = false;
+container.addEventListener('mousedown', () => {
+    isMouseDown = true;
+});
+document.addEventListener('mouseup', () => {
+    isMouseDown = false;
+});
+container.addEventListener('mousemove', (event) => {
+    if (isMouseDown) {
+        const chosenColour = picker.value;
+        if (event.target.classList.contains('grid-item')) {
+            event.target.style.backgroundColor = chosenColour;
+        }
+    }
+});
 
 const gridItems = document.querySelectorAll('.grid-item');
 gridItems.forEach(gridItem=>{
@@ -45,4 +72,5 @@ toggleGridLines.addEventListener('click', () => {
     const gridContainer = document.querySelector('.grid-container');
     gridContainer.classList.toggle('hide-grid-lines');
 });
+
 
